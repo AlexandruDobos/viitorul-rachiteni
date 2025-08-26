dd130666ef98eb64d3bdf0b88726b31edfa85261
Raluca Antal-Vaida <ralucaantal0609@gmail.com>
2025-08-26 18:04:02 +0300

    added netty

---
+++ backend/gateway/src/main/java/config/NettyHttp2Config.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.HttpProtocol;
+
+@Configuration
+public class NettyHttp2Config {
+    @Bean
+    NettyReactiveWebServerFactory nettyReactiveWebServerFactory() {
+        NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
+        // Acceptă atât HTTP/1.1 cât și HTTP/2 cleartext (H2C)
+        factory.addServerCustomizers(httpServer ->
+                httpServer.protocol(HttpProtocol.HTTP11, HttpProtocol.H2C)
+        );
+        return factory;
+    }
+}

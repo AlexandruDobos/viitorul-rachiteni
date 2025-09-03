a7968770a9a6e74152465d0b415e0c337d7752a3
Raluca Antal-Vaida <ralucaantal0609@gmail.com>
2025-09-03 09:54:04 +0300

    security config changes

--- backend/gateway/src/main/java/config/SecurityConfig.java
+++ backend/gateway/src/main/java/config/SecurityConfig.java
@@ -3,9 +3,6 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -14,17 +11,11 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 @Profile("prod")
 public class SecurityConfig {
-
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
-                .cors(Customizer.withDefaults())          // activează CORS pe WebFlux
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(ex -> ex
-                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // preflight liber
-                        .anyExchange().permitAll()                            // lasă auth în servicii
-                )
+                .authorizeExchange(ex -> ex.anyExchange().permitAll())
                 .build();
     }
 }
-
